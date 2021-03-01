ee9d41ed66009d78894e42f7826e442487fffc01
Your Name <ramonsoesan@MacBook-Pro-van-Ramon.local>
2021-03-01 14:49:00 +0100

    walker

--- Ramon/lattice.py
+++ Ramon/lattice.py
@@ -15,6 +15,7 @@ class Lattice:
         self.dx = self.dy = 1/N
         self.lattice = np.zeros((self.N, self.N))
         self.objects = np.zeros((self.N, self.N))
+        self.walker = np.zeros((self.N, self.N))
         self.delta = 100
 
 
@@ -92,10 +93,173 @@ class Lattice:
         self.objects[int(new_object[0][0]), int(new_object[0][1])] = 1
         self.lattice[int(new_object[0][0]), int(new_object[0][1])] = 0
 
+    def random_walk(self):
+        """
+        Function that performs a random step.
+        """
+        # check if there is a walking object or there has to be initialized one
+        if len(np.where(np.array(self.walker) == 1)[0]) == 0:
+            self.initialize_walker()
+        else:
+            direction = self.direction()
+            current_i, current_j = self.current_position()
+            new_i, new_j = self.next_point(direction, current_i, current_j)
+            if self.check_sinks(new_i) == True:
+                self.initialize_walker()
+                return False
+            elif self.check_periodic(new_j) == True:
+                new_j = self.periodic_j(new_j)
+            if self.check_object(new_i, new_j) == True:
+                self.stick(new_i, new_j)
+                print("jaja bingo")
+                self.walker[current_i, current_j] = 0
+                self.lattice[new_i, new_j] = 0
+                return False
+            else:
+                self.do_step(new_i, new_j)
+                self.walker[current_i, current_j] = 0
+                return False
+
+
+    def initialize_walker(self):
+        """
+        Initialize a random walker object at the top boundary.
+        """
+
+        self.walker = np.zeros((self.N, self.N))
+
+        p = [1/self.N for i in range(self.N)]
+
+        new_object_index = np.random.choice([i for i in range(self.N)], 1, p=p)
+
+        self.walker[0, new_object_index] = 1
+
+    def direction(self):
+        """
+        Determine the direction of the step.
+        """
+
+        # lef, right, up down
+        directions = ['left', 'right', 'up', 'down']
+
+        p = [1/4 for i in range(len(directions))]
+        # p = [0,0,1,0]
+        step_direction = np.random.choice(directions, 1, p=p)
+
+        return step_direction
+
+    def current_position(self):
+        """
+        Det. current position.
+        """
+
+        current_position = np.where(self.walker == 1)
+
+        return current_position[0], current_position[1]
+
+    def next_point(self, direction, current_i, current_j):
+        """
+        Determine next grid point to walk to.
+        """
+
+        if direction == 'left':
+            new_i = current_i
+            new_j = current_j - 1
+
+        elif direction == 'right':
+            new_i = current_i
+            new_j = current_j + 1
+
+        elif direction == 'up':
+            new_i = current_i + 1
+            new_j = current_j
+
+        elif direction == 'down':
+            new_i = current_i - 1
+            new_j = current_j
+
+        return new_i, new_j
+
+    def check_sinks(self, new_i):
+        """
+        Check if new position if boundary
+        """
+
+        if new_i == self.N-1 or new_i == 0 or new_i == -1:
+            return True
+
+        else:
+            return False
+
+    def check_periodic(self, new_j):
+        """
+        Check if new point is at the left or right boundary.
+        """
+
+        if new_j == -1 or new_j == self.N:
+            return True
+        else:
+            return False
+
+    def periodic_j(self, new_j):
+        """
+        Change new j with periodic conditions.
+        """
+
+        if new_j == -1:
+            new_j = self.N - 1
+        elif new_j == self.N:
+            new_j = 0
+
+        # print(new_j)
+        return new_j
+
+    def check_object(self, new_i, new_j):
+        """
+        Check if walker neighbours object.
+        """
+
+        if new_j + 1 == self.N:
+            check_j = 1
+            if self.objects[new_i+1,new_j] == 1 or self.objects[new_i-1,new_j] == 1 or self.objects[new_i, check_j] == 1 or self.objects[new_i, new_j-1] == 1:
+                return True
+        elif new_j - 1 == -1:
+            check_j = 1
+            if self.objects[new_i+1,new_j] == 1 or self.objects[new_i-1,new_j] == 1 or self.objects[new_i, new_j+1] == 1 or self.objects[new_i, check_j] == 1:
+                return True
+
+        else:
+            if self.objects[new_i+1,new_j] == 1 or self.objects[new_i-1,new_j] == 1 or self.objects[new_i, new_j+1] == 1 or self.objects[new_i, new_j-1] == 1:
+                return True
+
+    def stick(self, new_i, new_j):
+        """
+        Stick walker to object.
+        """
+
+        self.objects[new_i, new_j] = 1
+
+    def do_step(self, new_i, new_j):
+        """
+        Perform step.
+        """
+        self.walker[new_i, new_j] = 1
+
     def print_lattice(self):
         fig, ax = plt.subplots()
         self.lattice[self.lattice==0] = np.nan
         ax.imshow(self.lattice, cmap="rainbow", interpolation='nearest', extent=[0,1,0,1], aspect='auto')
+        ax.set_aspect(1)
+
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.grid()
+        plt.show()
+
+    def print_object(self):
+        fig, ax = plt.subplots()
+        self.objects[self.objects==0] = np.nan
+        ax.imshow(self.objects, cmap="rainbow", interpolation='nearest', extent=[0,1,0,1], aspect='auto')
         ax.set_aspect(1)
 
         plt.xlabel('x')
--- Ramon/set2.py
+++ Ramon/set2.py
@@ -10,24 +10,26 @@ def main():
 
     # Initializing ojbect in the lattice (bacteria) and empty lattice:
     x = int(0.99*l.N)
+    x = 0.5*l.N
     y = int(0.49*l.N)
 
-    l.initial_lattice(x,y, 1, 1)             # Top row = 1, object in lattice
-
+    l.initial_lattice(int(x),y, 1, 90)             # Top row = 1, object in lattice
 
     epsilon = 10**-5
     num_iterations = 0
-    while(l.delta > epsilon):
-    # while(num_iterations < 1):
+    # while(l.delta > epsilon):
+    while(num_iterations < 2100):
         l.SOR(omega = 1)
-        # print(l.lattice)
-        l.grow_object(eta = 0.5)
+        # l.grow_object(eta = 0.2)
+        l.random_walk()
+        # print(l.walker)
         num_iterations+=1
 
-    # print(l.lattice[8,4])
-    # print(np.where(l.lattice == 0.0001283884048461914))
+    print(np.where(l.walker == 1))
+    # print(l.objects)
     print("Number of iterations needed to get here = ", num_iterations)
     l.print_lattice()
+    # l.print_object()
 
 if __name__ == "__main__":
     main()
